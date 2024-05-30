183b1cc6866111e976f6b6f7c74248f575fa0928
sofiavavruk <sofiiavavruk @gmail.com>
2024-05-31 00:53:56 +0300

    REQ-10: Add a few clients and print them (grouped by cities)

--- src/main/java/com/magicvet/component/ApplicationRunner.java
+++ src/main/java/com/magicvet/component/ApplicationRunner.java
@@ -1,47 +1,12 @@
 package main.java.com.magicvet.component;
 
-import main.java.com.magicvet.Main;
-import main.java.com.magicvet.model.Client;
-import main.java.com.magicvet.model.Pet;
-import main.java.com.magicvet.service.ClientService;
-import main.java.com.magicvet.service.PetService;
-
 public class ApplicationRunner {
-    private final ClientService clientService = new ClientService();
-    private final PetService petService = new PetService();
+
+    private final EnityRegister register = new EnityRegister();
     public void run(){
         if (Authenticator.auth()){
-            Client client = clientService.registerNewClient();
-
-            if (client != null){
-                registerPet(client);
-                System.out.println(client);
-            }
+            register.registerClients();
         }
     }
 
-    private void registerPet(Client client){
-        boolean continueAddPet = true;
-        while (continueAddPet){
-            addPet(client);
-
-            System.out.print("Do you want to add more pets for the current client? (y/n)");
-            String answer = Main.SCANNER.nextLine();
-
-            if("n".equals(answer)){     //continueAddPet = !"n".equals(answer); or continueAddPet = "y".equals(answer);
-                continueAddPet = false;
-            }
-        }
-    }
-
-    private void addPet(Client client){
-        System.out.println("Adding a new pet. ");
-        Pet pet  = petService.registerNewPet();
-
-        if (pet != null) {
-            client.addPet(pet);
-            pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
-            System.out.println("Pet has been added. ");
-        }
-    }
 }
--- src/main/java/com/magicvet/model/Client.java
+++ src/main/java/com/magicvet/model/Client.java
@@ -14,11 +14,13 @@ public class Client {
     private String email;
     private List<Pet> pets = new ArrayList<>();
     private final LocalDateTime registrationDate = LocalDateTime.now();
+    private Location location;
 
     @Override
     public String toString(){
         return "Client {" + "\n\tfirstName = " + firstName +
                 ", lastName = " + lastName + ", email = " + email +
+                ", location = " + location +
                 ",\n\tpets = " + pets + "\n\tregistrationDate = " + registrationDate.format(FORMATTER) + "\n}";
     }
 
@@ -57,15 +59,27 @@ public class Client {
         return email;
     }
 
-    public List<Pet> getPet() {
+    public List<Pet> getPets() {
         return pets;
     }
 
-    public void setPet(List<Pet> pets) {
+    public void setPets(List<Pet> pets) {
         this.pets = pets;
     }
 
     public void addPet(Pet pet){
         pets.add(pet);
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public enum Location{
+        KYIV, LVIV, ODESA
     }
 }
